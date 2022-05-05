5de0a51ff512cd9a10fd6e4138d73e095270ff5f
ariffugur <ariffugur@gmail.com>
2022-05-05 19:14:26 +0300

    Departman Modulu

---
+++ DevExpressTeknikServis/Formlar/FrmDepartman.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevExpressTeknikServis.Formlar
+{
+    public partial class FrmDepartman : Form
+    {
+        public FrmDepartman()
+        {
+            InitializeComponent();
+        }
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        private void FrmDepartman_Load(object sender, EventArgs e)
+        {
+            gridControl1.DataSource=db.TBLDEPARTMAN.ToList();
+            labelControl12.Text=db.TBLDEPARTMAN.Count().ToString();
+            labelControl18.Text=db.TBLPERSONEL.Count().ToString();
+        }
+    }
+}
