56a41faef7ce561b9a9aabc418b5b87c0fb1dd57
Enes Celik <eneesceliikk@hotmail.com>
2022-05-20 04:28:04 +0300

    Arizali Urun Kaydi Guncelleme

---
+++ DevExpressTeknikServis/Formlar/FrmArizaDetaylar.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DevExpressTeknikServis.Formlar
+{
+    partial class FrmArizaDetaylar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmArizaDetaylar));
+            this.btnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.richTxtBoxArizaDetay = new System.Windows.Forms.RichTextBox();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.txtSeriNo = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.txtTarih = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSeriNo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTarih.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnKayitYap.ImageOptions.Image")));
+            this.btnGuncelle.Location = new System.Drawing.Point(12, 319);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(108, 38);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
+            this.labelControl1.Location = new System.Drawing.Point(12, 12);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(213, 21);
+            this.labelControl1.TabIndex = 7;
+            this.labelControl1.Text = "Arızalı Ürün Kaydı Açıklama";
+            // 
+            // richTxtBoxArizaDetay
+            // 
+            this.richTxtBoxArizaDetay.Location = new System.Drawing.Point(12, 202);
+            this.richTxtBoxArizaDetay.Name = "richTxtBoxArizaDetay";
+            this.richTxtBoxArizaDetay.Size = new System.Drawing.Size(335, 96);
+            this.richTxtBoxArizaDetay.TabIndex = 8;
+            this.richTxtBoxArizaDetay.Text = "";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.Appearance.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Appearance.Options.UseForeColor = true;
+            this.labelControl2.Location = new System.Drawing.Point(12, 66);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(40, 17);
+            this.labelControl2.TabIndex = 9;
+            this.labelControl2.Text = "Seri No";
+            // 
+            // txtSeriNo
+            // 
+            this.txtSeriNo.Location = new System.Drawing.Point(12, 89);
+            this.txtSeriNo.Name = "txtSeriNo";
+            this.txtSeriNo.Properties.MaskSettings.Set("MaskManagerType", typeof(DevExpress.Data.Mask.SimpleMaskManager));
+            this.txtSeriNo.Properties.MaskSettings.Set("MaskManagerSignature", "ignoreMaskBlank=True");
+            this.txtSeriNo.Properties.MaskSettings.Set("mask", "99999");
+            this.txtSeriNo.Size = new System.Drawing.Size(100, 20);
+            this.txtSeriNo.TabIndex = 10;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.Appearance.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Appearance.Options.UseForeColor = true;
+            this.labelControl3.Location = new System.Drawing.Point(12, 115);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(27, 17);
+            this.labelControl3.TabIndex = 11;
+            this.labelControl3.Text = "Tarih";
+            // 
+            // txtTarih
+            // 
+            this.txtTarih.Location = new System.Drawing.Point(12, 138);
+            this.txtTarih.Name = "txtTarih";
+            this.txtTarih.Properties.MaskSettings.Set("MaskManagerType", typeof(DevExpress.Data.Mask.DateTimeMaskManager));
+            this.txtTarih.Properties.MaskSettings.Set("mask", "d");
+            this.txtTarih.Size = new System.Drawing.Size(100, 20);
+            this.txtTarih.TabIndex = 12;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl4.Appearance.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Appearance.Options.UseForeColor = true;
+            this.labelControl4.Location = new System.Drawing.Point(12, 179);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(79, 17);
+            this.labelControl4.TabIndex = 13;
+            this.labelControl4.Text = "Arıza Detayları";
+            // 
+            // FrmArizaDetaylar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(389, 399);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.txtTarih);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.txtSeriNo);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.richTxtBoxArizaDetay);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.btnGuncelle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmArizaDetaylar";
+            this.Text = "FrmArizaDetaylar";
+            ((System.ComponentModel.ISupportInitialize)(this.txtSeriNo.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTarih.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btnGuncelle;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.RichTextBox richTxtBoxArizaDetay;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtSeriNo;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit txtTarih;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+    }
+}
